6ef26689f5291f14362ec86b244bf51aaec40a47
Philip Riley <hiriley510@gmail.com>
2015-02-08 20:47:16 -0800

    Taking out dumb animation

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
 
-  $("#personal-intro").css({"opacity":"0"})
-  $("#index-title").css({"opacity":"0"})
-
-  $("#image img").css({"height":"0px"});
-  $("#image img").animate({"height":"500px"}, 700, function() {
-    $("#index-title").animate({"opacity":"1"},500);
-    $("#personal-intro").animate({"opacity":"1"}, 500);
-  });
+  // $("#personal-intro").css({"opacity":"0"})
+  // $("#index-title").css({"opacity":"0"})
 
   // $("#image img").css({"opacity":"0"})
-  //   $("#image img").animate({"opacity":"1"},500, function(){
-  //     $("#index-title").animate({"opacity":"1"},700);
-  //     $("#personal-intro").animate({"opacity":"1"},700);
-  //   })
+
+  //   $("#image img").animate({"opacity":"1"},800);
+  //   $("#index-title").animate({"opacity":"1"},800);
+  //   $("#personal-intro").animate({"opacity":"1"},800);
+
+
+//  ANIMATE HEIGHT UP________
+  // $("#image img").css({"height":"0px"});
+  // $("#image img").animate({"height":"500px"}, 700, function() {
+  //   $("#index-title").animate({"opacity":"1"},500);
+  //   $("#personal-intro").animate({"opacity":"1"}, 500);
+  // });
+// END ANIMATE HEIGHT UP_________
+
+
 
   $("#nav-bar ul li").mouseenter(function() {
     $(this).animate({"font-size":"+=0.5em"})
